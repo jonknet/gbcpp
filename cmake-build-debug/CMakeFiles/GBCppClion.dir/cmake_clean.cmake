af512d0afadf0e5f0f39083b0e73ae50ae9da8ff
jonknet <jonknet@gmail.com>
2021-07-08 17:59:38 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 95ae30ec556adb079e522fd806f2a5442b7c987f, reversing
    changes made to 09e0b01d96acc058a97b050d69d5e2c849a8a9c1.

---
+++ cmake-build-debug/CMakeFiles/GBCppClion.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GBCppClion.dir/cpu.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/cpuimpl.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/main.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/mm.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/ops.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/ppu.cpp.obj"
+  "CMakeFiles/GBCppClion.dir/utils.cpp.obj"
+  "GBCppClion.exe"
+  "GBCppClion.exe.manifest"
+  "GBCppClion.lib"
+  "GBCppClion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GBCppClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
